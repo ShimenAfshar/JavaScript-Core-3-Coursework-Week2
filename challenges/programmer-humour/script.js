5fad61346f883b1200fd71cbc33d325f1544f147
ShimenAfshar <afsharshimen@gmail.com>
2022-11-08 11:41:29 +0000

    programmer add li

--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -9,3 +9,11 @@ const getImage = () => {
       }
       return response.json();
     })
+     .then((data) => {
+      const liElement = document.createElement("li");
+      liElement.innerHTML = `<div id='container'>
+            <div class='container'>
+            <div class='img_div'><img src='${data.img}'/><div>
+            
+            </div>
+            </div>`;
